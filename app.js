39893c92b1a69614b988744b75e128c102eecc11
Prashant Patil <97236302+Prashantp97@users.noreply.github.com>
2022-11-21 16:21:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+
+months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+  let firstDay = new Date(year, month).getDay();
+
+  tbl = document.getElementById("calendar-body");
+
+  tbl.innerHTML = "";
+  console.log(tbl);
+
+  selectYear.value = year;
+  selectMonth.value = month;
+
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        cell = document.createElement("td");
+
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+        cell = document.createElement("td");
+        cellText = document.createTextNode(date);
+        if (
+          date === today.getDate() &&
+          year === today.getFullYear() &&
+          month === today.getMonth()
+        ) {
+        }
+        cell.appendChild(cellText);
+        cell.className = date;
+        row.appendChild(cell);
+        date++;
+      }
+    }
+
+    tbl.appendChild(row);
+  }
+}
+
+function daysInMonth(iMonth, iYear) {
+  return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+
+function find() {
+  var userInput = document.getElementById("inputbox").value;
+  if (userInput != "") {
+    var requriedBox = document.getElementsByClassName(userInput)[0];
+    var previousBox = requriedBox.classList.contains("setColor");
+
+    if (previousBox) {
+      requriedBox.classList.remove("setColor");
+    } else {
+      requriedBox.classList.add("setColor");
+    }
+  }
+}
